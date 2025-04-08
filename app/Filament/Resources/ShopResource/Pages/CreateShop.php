76719c37a512f4f392ed14a7b0e0ac76e18c720e
ayumi <jackie19980824@gmail.com>
2025-04-09 00:57:10 +0900

    commit14

--- app/Filament/Resources/ShopResource/Pages/CreateShop.php
+++ app/Filament/Resources/ShopResource/Pages/CreateShop.php
@@ -11,11 +11,11 @@ class CreateShop extends CreateRecord
 {
     protected static string $resource = ShopResource::class;
 
-    public static function mutateFormDataBeforeCreate(array $data): array
+    public function mutateFormDataBeforeCreate(array $data): array
     {
-    // 保存前にログを出力して、imageキーが入っているか確認
-    Log::debug('mutateFormDataBeforeCreate', $data);
+        // 保存前にログを出力して、imageキーが入っているか確認
+        Log::debug('mutateFormDataBeforeCreate', $data);
 
-    return $data;
+        return $data;
     }
 }
