280ef46916852e03dc7972017dc42bfbc2fac6f9
ayumi <jackie19980824@gmail.com>
2025-04-09 00:44:42 +0900

    commit13

--- app/Filament/Resources/ShopResource/Pages/CreateShop.php
+++ app/Filament/Resources/ShopResource/Pages/CreateShop.php
@@ -5,8 +5,17 @@ namespace App\Filament\Resources\ShopResource\Pages;
 use App\Filament\Resources\ShopResource;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Log;
 
 class CreateShop extends CreateRecord
 {
     protected static string $resource = ShopResource::class;
+
+    public static function mutateFormDataBeforeCreate(array $data): array
+    {
+    // 保存前にログを出力して、imageキーが入っているか確認
+    Log::debug('mutateFormDataBeforeCreate', $data);
+
+    return $data;
+    }
 }
