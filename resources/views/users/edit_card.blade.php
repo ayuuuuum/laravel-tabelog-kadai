9bf25298ca865a5d4c5241feea33776226ea3e48
ayumi <jackie19980824@gmail.com>
2025-04-10 19:35:57 +0900

    commit32

--- resources/views/users/edit_card.blade.php
+++ resources/views/users/edit_card.blade.php
@@ -8,9 +8,16 @@
         @csrf
 
         <label for="card-holder-name">カード名義</label>
-        <input id="card-holder-name" type="text" class="form-control">
+            <input id="card-holder-name" type="text" class="form-contro mb-3">
 
-        <div id="card-element"></div>
+        <label>カード番号</label>
+        <div id="card-number-element" class="form-control mb-3"></div>
+
+        <label>有効期限</label>
+        <div id="card-expiry-element" class="form-control mb-3"></div>
+
+        <label>CVC</label>
+        <div id="card-cvc-element" class="form-control mb-3"></div>
 
         <input type="hidden" id="payment-method" name="payment_method">
         <button id="card-button" class="btn btn-primary mt-3">更新する</button>
@@ -20,6 +27,42 @@
 <script src="https://js.stripe.com/v3/"></script>
 <script>
     document.addEventListener("DOMContentLoaded", async function () {
+    const stripe = Stripe("{{ env('STRIPE_KEY') }}");
+    const elements = stripe.elements();
+
+    const cardNumber = elements.create('cardNumber');
+    const cardExpiry = elements.create('cardExpiry');
+    const cardCvc = elements.create('cardCvc');
+
+    cardNumber.mount('#card-number-element');
+    cardExpiry.mount('#card-expiry-element');
+    cardCvc.mount('#card-cvc-element');
+
+    const form = document.getElementById('card-update-form');
+    const cardButton = document.getElementById('card-button');
+    const cardHolderName = document.getElementById('card-holder-name');
+    const paymentMethodInput = document.getElementById('payment-method');
+
+    cardButton.addEventListener('click', async function (event) {
+        event.preventDefault();
+
+        const { paymentMethod, error } = await stripe.createPaymentMethod({
+            type: 'card',
+            card: cardNumber, // ここだけ渡せばOK（内部的にexpiryとcvcもまとめて扱われる）
+            billing_details: {
+                name: cardHolderName.value
+            },
+        });
+
+        if (error) {
+            alert(error.message);
+        } else {
+            paymentMethodInput.value = paymentMethod.id;
+            form.submit();
+        }
+    });
+});
+    {{--document.addEventListener("DOMContentLoaded", async function () {
         const stripe = Stripe("{{ env('STRIPE_KEY') }}");
         const elements = stripe.elements();
         const cardElement = elements.create('card', { hidePostalCode: true });
@@ -44,6 +87,6 @@
                 form.submit();
             }
         });
-    });
+    });--}}
 </script>
 @endsection
